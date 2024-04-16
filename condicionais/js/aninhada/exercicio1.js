68ee8c3df674b7818e0a783e5262fef77a430886
santos869458 <maria.d.santos37@aluno.senai.br>
2024-04-16 07:22:00 -0300

    modificado

---
+++ condicionais/js/aninhada/exercicio1.js
@@ -0,0 +1,6 @@
+var idade = 18
+if (idade < 16) {
+    console.log('Não vota!')
+} else {
+    console.log('vota!')
+}
---
+++ condicionais/js/aninhada/exercicio1_2.js
@@ -0,0 +1,10 @@
+var idade = 18
+if (idade < 16){
+    console.log('Não vota!')
+} else {
+    if(idade >= 18 || idade <65) {     
+        console.log('voto opcional!')
+    }
+} else {
+    console.log('Voto obrigatorio')
+}
